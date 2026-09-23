2dbdc33acec06041dedc075e022f530bba444be0
Wasem Twfek <121197063+wasem15@users.noreply.github.com>
2026-09-23 20:07:59 +0300

    security: move PHP database credentials to environment

--- sign/db_connect.php
+++ sign/db_connect.php
@@ -1,12 +1,14 @@
 <?php
-/* Database connection start */
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "artfolio_db";
-$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+
+$servername = getenv('ARTFOLIO_DB_HOST') ?: 'localhost';
+$username = getenv('ARTFOLIO_DB_USER') ?: '';
+$password = getenv('ARTFOLIO_DB_PASSWORD') ?: '';
+$dbname = getenv('ARTFOLIO_DB_NAME') ?: 'artfolio_db';
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    error_log('Artfolio database connection failed: ' . mysqli_connect_error());
+    http_response_code(500);
+    exit('Database connection unavailable.');
 }
-?>
